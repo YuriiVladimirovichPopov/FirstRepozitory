a08b802bf320cf1ff217f9b095c6f53bad801365
Yurii Popov <papanchik87@yahoo.com>
2023-03-31 00:22:34 +0500

    bygaga.changed

---
+++ back-end js/bygaga1 changed.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+let requestsCount = 0
+
+const server = http.createServer((request, response) => {
+
+    requestsCount ++
+    if(request.url === '/favicon.ico'){
+        requestsCount --;
+    }
+    
+
+    switch (request.url) {
+        case '/students':
+        response.write('students ')
+        break;
+        case '/kids':
+            response.write('kids are back-end creaters ')
+            break;
+            default:
+                response.write('404 not found ')
+
+    }   
+    response.write('bygagashechki:' + requestsCount)
+    response.end()
+})
+
+server.listen(3003)
