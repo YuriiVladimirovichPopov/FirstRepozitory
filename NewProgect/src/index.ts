5297c21d69d4741e1f28c960f4793ee42e0b4db9
Yurii Popov <papanchik87@yahoo.com>
2023-04-02 17:28:29 +0500

    new progect

--- NewProgect/src/index.ts
+++ NewProgect/src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('my lovemaza fucka!!!')
+  res.send('my lovemaza fucka1!!!')
 })
 
 app.listen(port, () => {
